8d191104702240b91acc69543e806c515e7d4d6b
imohammm1 <stylestentionte@gmail.com>
2024-07-11 14:06:34 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -3,7 +3,7 @@ console.log("JS File was loaded");
 // 35.217665 N, 80.831473 W -> 35.217665, -0.831473
 
 let map = L.map("map", {
-  center: [35.217665, -0.831473],
+  center: [35.217665, -80.831473],
   zoom: 8
 });
 
