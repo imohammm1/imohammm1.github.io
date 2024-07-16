4610fd9d53fdb7793bb4b5d7ab081e5377bc78b9
imohammm1 <stylestentionte@gmail.com>
2024-07-16 13:28:15 -0400

    Update netherlands.js

--- netherlands.js
+++ netherlands.js
@@ -1,9 +1,9 @@
 console.log("JS File was loaded");
 
-// 35.217665 N, 80.831473 W -> 35.2176665, -0.831473
+// 52.1326 N, 5.2913 E -> 52.1326, 5.2913
 
 let map = L.map("map", {
-  center: [35.2176665, -80.831473],
+  center: [52.1326, 5.2913],
   zoom: 15
 });
 
@@ -12,8 +12,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 let marker = L.marker(
-  [35.2176665, -80.831473], {
-    title: "CPCC Central Campus"
+  [52.1326, 5.2913], {
+    title: "The Netherlands"
   }
   ).bindPopup(`<h2 class="colorOne">Hello from CPCC!</h2><hr><iframe width="280" height="120" src="https://www.youtube.com/embed/3ymn7BxITrA" title="Central Piedmont - Community is Who We Are (:15)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`)
   .addTo(map);
