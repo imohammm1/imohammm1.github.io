88a5698689176d74198039b45ed3438160c2cb9d
imohammm1 <stylestentionte@gmail.com>
2024-07-16 14:20:11 -0400

    Update netherlands.js

--- netherlands.js
+++ netherlands.js
@@ -11,6 +11,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// keukenhof park: Coordinates: 52.269386 N, 4.548342 E -> 52.269386, 4.548342
 let circle = L.circle([52.269386, 4.548342], {
     color: '#402e21',
     fillColor: '#9c7356',
