afe27c9511141a0aecda54093e26ce94cd6082c7
imohammm1 <stylestentionte@gmail.com>
2024-07-18 14:01:32 -0400

    Update earthquakes.js

--- earthquakes.js
+++ earthquakes.js
@@ -1,3 +1,10 @@
 console.log("JS File was loaded")
 
 let url= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+d3.json(url).then(
+  function(data)
+  {
+    console.log(data);
+  }
+);
