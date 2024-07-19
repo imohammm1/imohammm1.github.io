63bc7fbd325438d93deb21ba5cb3656c79c922de
imohammm1 <stylestentionte@gmail.com>
2024-07-19 13:40:50 -0400

    Update inputDemo.js

--- inputDemo.js
+++ inputDemo.js
@@ -9,8 +9,5 @@ function outputDemo()
   alert(output);
 
   document.getElementById("output").innerHTML = "You entered: " + output;
-
-  if (document.getElementById("myText").value = "im stupid"){
-    console.log("youre stupid");
   }
 }
