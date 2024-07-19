c24c0c47682a4ec30480d1ab85d5f5d427482691
imohammm1 <stylestentionte@gmail.com>
2024-07-19 14:53:19 -0400

    Update earthquakes.js

--- earthquakes.js
+++ earthquakes.js
@@ -11,12 +11,13 @@ d3.json(url).then(
     let map = L.map("map", {
   center: [37.09, -95.71],
   zoom: 5
-});
+	});
 
-var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-}).addTo(map);
-  function onEachFeature(feature, layer)
+    var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+	}).addTo(map);
+}
+function onEachFeature(feature, layer)
       {
         layer.bindPopup(`<center>
         <h3>${feature.properties.place}</h3>
@@ -25,8 +26,6 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
         <p><b>Magnitude</b> ${feature.properties.mag.toFixed(2)}</p>
         </center>`);
       }
-
     let earthquakes = L.geoJSON(data.features, {
       onEachFeature: onEachFeature
     }).addTo(map);	
-}
