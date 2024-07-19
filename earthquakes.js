88eb566f9ddad4453b5b0edcfee008e0a2a1cf5f
imohammm1 <stylestentionte@gmail.com>
2024-07-19 14:58:24 -0400

    Update earthquakes.js

--- earthquakes.js
+++ earthquakes.js
@@ -16,7 +16,6 @@ d3.json(url).then(
     var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 	}).addTo(map);
-}
 function onEachFeature(feature, layer)
       {
         layer.bindPopup(`<center>
